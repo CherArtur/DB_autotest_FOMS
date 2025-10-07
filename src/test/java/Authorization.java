a481aeb03a77e56c5cf1709b87d843558b132c1a
CherArtur <cherepanovartur@gmail.com>
2025-10-07 23:43:04 +0500

    one commit and push

--- src/test/java/Authorization.java
+++ src/test/java/Authorization.java
@@ -1,2 +1,8 @@
+package guru.qa;
 public class Authorization {
+    private static String msg = "Hello qa.guru!";
+
+    public static void main(String... args) {
+        System.out.println(msg);
+    }
 }
